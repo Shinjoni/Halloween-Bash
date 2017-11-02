61dc06e6738ca81f58640b5a7946c7969d22e0e4
alexander-schuetz <32182060+alexander-schuetz@users.noreply.github.com>
2017-11-02 19:06:30 -0400

    Caves

---
+++ Assets/Assets/Slime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class Slime : MonoBehaviour {
+	private float volume;
+	public float speed;
+	Rigidbody2D slimeBody;
+	private int direction;
+	public Transform playerLocation;
+	public Transform slimeLocation;
+
+	AudioSource audio;
+	public AudioClip slimeScream;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		volume = 1 / (Mathf.Abs(slimeLocation.position.x - playerLocation.position.x));
+		transform.position = new Vector2 (transform.position.x + speed, transform.position.y);
+		audio.PlayOneShot (slimeScream, volume);
+	}
+}
---
+++ Assets/Assets/SpiderSense.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SpiderSense : MonoBehaviour {
+	public Rigidbody2D spiderBody;
+	// Use this for initialization
+	AudioSource audio;
+	public AudioClip ambient;
+	public AudioClip attack;
+	private Vector3 spiderStart;
+
+	void Start () {
+		spiderBody.constraints = RigidbodyConstraints2D.FreezePositionY;
+		spiderStart = transform.position;
+	}
+			
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+			spiderBody.constraints = RigidbodyConstraints2D.None | RigidbodyConstraints2D.FreezeRotation;
+			StartCoroutine (Delay());
+		}
+	}
+
+	IEnumerator Delay()
+	{
+		yield return new WaitForSeconds(5);
+		transform.position = spiderStart;
+		spiderBody.constraints = RigidbodyConstraints2D.FreezePositionY;
+	}
+}
